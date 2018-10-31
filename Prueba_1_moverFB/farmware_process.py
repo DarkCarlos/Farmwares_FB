035560ea6bd7b298fb9f8aa1fcc465f1a9ebd63e
DarkCarlos <41303631+DarkCarlos@users.noreply.github.com>
2018-10-31 11:11:06 -0500

    Add files via upload
    0.0.1 - comparar areas

--- Prueba_1_moverFB/farmware_process.py
+++ Prueba_1_moverFB/farmware_process.py
@@ -36,6 +36,21 @@ class MyFarmware():
             location=[0, 0, 0],
             offset=[0, 0, 0],
             speed=800)
+
+    def mov_right(self): """ si detecta que es menor a X se va a la derecha"""
+        log('Execute move: ', message_type='debug', title=str(self.farmwarename))
+        move_absolute(
+            location=[300, 400, 0],
+            offset=[0, 0, 0],
+            speed=800)
+
+    def mov_left(self): """ si detecta que es mayor a X se va a la izquierda"""
+        log('Execute move: ', message_type='debug', title=str(self.farmwarename))
+        move_absolute(
+            location=[300, 700, 0],
+            offset=[0, 0, 0],
+            speed=800)
+
     def mov_robot_photo(self):
         log('Execute move: ', message_type='debug', title=str(self.farmwarename))
         move_absolute(
@@ -51,62 +66,13 @@ class MyFarmware():
         self.plant_detection = PlantDetection(coordinates=True, app=True)
         self.plant_detection.detect_plants()
 
-    def label(self, p2c=None, weeder_remove=False, weeder_safe_remove=False):
-        """Draw circles on image indicating detected plants."""
-        def _circle(objects, color, already_pixels=False):
-            bgr = {'red': (0, 0, 255),
-                   'green': (0, 255, 0),
-                   'blue': (255, 0, 0),
-                   'cyan': (255, 255, 0),
-                   'grey': (200, 200, 200)}
-            if not already_pixels:
-                pixel_objects = p2c.convert(objects, to_='pixels')
-            else:
-                pixel_objects = objects
-            for obj in pixel_objects:
-                cv2.circle(self.images['marked'], (int(obj[0]), int(obj[1])),
-                           int(obj[2]), bgr[color], CIRCLE_LINEWIDTH)
-
-        if p2c is None:
-            detected_pixel_objects = self.plant_db.pixel_locations
-            _circle(detected_pixel_objects, 'red', already_pixels=True)
-        else:
-            # Mark known plants
-            known = [[_['x'], _['y'], _['radius']] for _
-                     in self.plant_db.plants['known']]
-            _circle(known, 'green')
-
-            # Mark weeds
-            remove = [[_['x'], _['y'], _['radius']] for _
-                      in self.plant_db.plants['remove']]
-            _circle(remove, 'red')
-
-            # Mark weeder size for weeds
-            if weeder_remove:
-                weeder_size = self.plant_db.weeder_destrut_r
-                remove_circle = [[_['x'], _['y'], weeder_size] for _
-                                 in self.plant_db.plants['remove']]
-                _circle(remove_circle, 'grey')
-
-            # Mark saved plants
-            save = [[_['x'], _['y'], _['radius']] for _
-                    in self.plant_db.plants['save']]
-            _circle(save, 'blue')
-
-            # Mark safe-remove weeds
-            safe_remove = [[_['x'], _['y'], _['radius']] for _
-                           in self.plant_db.plants['safe_remove']]
-            _circle(safe_remove, 'cyan')
-
-            # Mark weeder size for safe-remove weeds
-            if weeder_safe_remove:
-                weeder_size = self.plant_db.weeder_destrut_r
-                safe_remove_circle = [[_['x'], _['y'], weeder_size] for _
-                                      in self.plant_db.plants['safe_remove']]
-                _circle(safe_remove_circle, 'grey')
-
     def run(self):
         self.mov_robot_origin()
         self.mov_robot_photo()
-        self.label(p2c=none,weeder_remove=False,weeder_safe_remove=False)
+        self.take_photo()
+        if cv2.contourArea(cnt) > 1:
+            self_mov_right
+            valid_contours.append(cnt)
+        else:
+            self_mov_left
         sys.exit(0)
